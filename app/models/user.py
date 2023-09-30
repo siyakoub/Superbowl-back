2cbc927876bd4291e8c7f8fbcacfd760f0fb4e42
siyakoub <55182742+siyakoub@users.noreply.github.com>
2023-09-30 03:03:43 +0100

    Correction de la class User, Création et test effectué pour la class User

--- app/models/user.py
+++ app/models/user.py
@@ -2,29 +2,39 @@ from app import mysql
 
 
 class User:
-    def __init__(self, nom, prenom, adresseEmail, motDePasse):
+    def __init__(self, nom, prenom, adresseEmail, motDePasse, userID=None, dateInscription=None):
+        self.userID = userID
         self.nom = nom
         self.prenom = prenom
         self.adresseEmail = adresseEmail
         self.motDePasse = motDePasse
+        self.dateInscription = dateInscription
 
     def save(self):
         conn = mysql.connect()
         cursor = conn.cursor()
         cursor.execute(
-            "INSERT INTO Utilisateur (nom, prenom, adresseEmail, motDePasse) VALUES (%s, %s, %s, %s)",
+            "CALL sp_createUser(%s, %s, %s, %s)",
             (self.nom, self.prenom, self.adresseEmail, self.motDePasse)
         )
-        mysql.connection.commit()
+        conn.commit()
         cursor.close()
 
     @staticmethod
-    def get_by_id(user_id):
+    def get_by_email(email):
         conn = mysql.connect()
         cursor = conn.cursor()
-        cursor.execute("SELECT * FROM Utilisateur WHERE userID = %s", (user_id,))
+        cursor.execute("SELECT * FROM Utilisateur WHERE adresseEmail = %s", (email,))
         user_data = cursor.fetchone()
         cursor.close()
         if user_data:
-            return User(*user_data)
-        return None
+            user_id = int(user_data[0])
+            user_name = user_data[1]
+            user_prenom = user_data[2]
+            user_email = user_data[3]
+            user_password = user_data[4]
+            user_dateInscription = user_data[5]
+            return User(user_name, user_prenom, user_email, user_password, user_id, user_dateInscription)
+        else:
+            return None
+
--- test/test_user.py
+++ test/test_user.py
@@ -1,7 +1,5 @@
 import unittest
-
 from flask import Flask
-
 from app.models.user import User
 from config import Config
 from flaskext.mysql import MySQL
@@ -17,13 +15,23 @@ class TestUser(unittest.TestCase):
         app.config['MYSQL_DATABASE_DB'] = Config.MYSQL_DB
 
         # Initialisation de l'extension Flask-MySQL
-        mysql = MySQL()
-        mysql.init_app(app)
+        self.mysql = MySQL()
+        self.mysql.init_app(app)
 
+        # Créez un contexte d'application pour les tests
+        self.app = app.test_client()
+        self.app_context = app.app_context()
+        self.app_context.push()
+
+        # Créez une connexion à la base de données dans le contexte de test
+        self.connection = self.mysql.connect()
+        self.cursor = self.connection.cursor()
 
     def tearDown(self):
-        # Vous pouvez fermer la connexion à la base de données de test ici si nécessaire
-        pass
+        # Fermez la connexion à la base de données de test ici si nécessaire
+        self.cursor.close()
+        self.connection.close()
+        self.app_context.pop()
 
     def test_user_creation(self):
         # Testez la création d'un utilisateur
@@ -31,11 +39,19 @@ class TestUser(unittest.TestCase):
         user.save()
 
         # Vérifiez si l'utilisateur a été correctement enregistré dans la base de données
-        fetched_user = User.get_by_id(user.userID)
+        fetched_user = User.get_by_email("john@example.com")
+        print(fetched_user)  # Ajoutez cette ligne pour afficher fetched_user dans la console
         self.assertIsNotNone(fetched_user)
+        print(fetched_user.userID)  # Imprimez userID après les autres informations
+        print(fetched_user.nom)
+        print(fetched_user.prenom)
+        print(fetched_user.adresseEmail)
+        print(fetched_user.motDePasse)
+        print(fetched_user.dateInscription)
+        self.assertEqual(fetched_user.userID, 5)
         self.assertEqual(fetched_user.nom, "John")
         self.assertEqual(fetched_user.prenom, "Doe")
         self.assertEqual(fetched_user.adresseEmail, "john@example.com")
         self.assertEqual(fetched_user.motDePasse, "password")
-
+        #print(fetched_user.dateInscription)
     # Ajoutez d'autres méthodes de test ici pour d'autres fonctionnalités de la classe User
